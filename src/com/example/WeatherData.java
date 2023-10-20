0d984ad6921b08a4ce3666da21351b5b8bce897b
sukeypark <hspark@data-alliance.com>
2023-10-20 15:16:51 +0900

    use variables

--- src/com/example/WeatherData.java
+++ src/com/example/WeatherData.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import com.example.display.CurrentConditionsDisplay;
+import com.example.display.Displayable;
+import com.example.display.ForecastDisplay;
+import com.example.display.StatisticsDisplay;
 import com.example.sensor.HumiditySensor;
 import com.example.sensor.PressureSensor;
 import com.example.sensor.Sensor;
@@ -11,8 +15,19 @@ public class WeatherData {
     Sensor pressureSensor = new PressureSensor();
     Sensor humiditySensor = new HumiditySensor();
 
-    public void measurementsChanged() {
+    // display
+    Displayable currentConditionsDisplay = new CurrentConditionsDisplay();
+    Displayable forecastDisplay = new ForecastDisplay();
+    Displayable statisticsDisplay = new StatisticsDisplay();
 
+    public void measurementsChanged() {
+        float temp = getTemperature();
+        float humidity = getHumidity();
+        float pressure = getPressure();
+
+        currentConditionsDisplay.update(temp, humidity, pressure);
+        forecastDisplay.update(temp, humidity, pressure);
+        statisticsDisplay.update(temp, humidity, pressure);
     }
 
     public float getTemperature() {
