4b18986cf3d881cb60607a9df9ce1bfffe1a9ceb
sukeypark <hspark@data-alliance.com>
2023-10-23 19:26:24 +0900

    implemebt observer

--- src/com/example/observer/ForecastDisplay.java
+++ src/com/example/observer/ForecastDisplay.java
@@ -1,11 +1,23 @@
 package com.example.observer;
 
+import com.example.Subject;
+
 public class ForecastDisplay implements Observer {
+
+    private Subject weatherData;
+    private float temp;
+    private float humidity;
+    private float pressure;
+
+    public ForecastDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+    }
 
     @Override
     public void update(float temp, float humidity, float pressure) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        this.temp = temp;
+        this.humidity = humidity;
+        this.pressure = pressure;
     }
 
 }
