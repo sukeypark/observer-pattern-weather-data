daa2bf496fff8aac0c5e2ff3801facf318b67412
sukeypark <hspark@data-alliance.com>
2023-10-24 15:53:26 +0900

    add WeatherStation

--- src/com/example/WeatherStation.java
+++ src/com/example/WeatherStation.java
@@ -1,9 +1,23 @@
 package com.example;
 
+import com.example.observer.CurrentConditionsDisplay;
+import com.example.observer.ForecastDisplay;
+import com.example.observer.Observer;
+import com.example.observer.StatisticsDisplay;
+
 public class WeatherStation {
 
-    public void measureData() {
-        /* codes for measuring data */
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        Observer currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        Observer forecastDisplay = new ForecastDisplay(weatherData);
+        Observer statisticsDisplay = new StatisticsDisplay(weatherData);
+
+        while (true) {
+            weatherData.setMeasurements(0, 0, 0);
+        }
+
     }
 
 }
