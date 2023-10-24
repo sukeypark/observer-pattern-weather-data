11ff8554383e515ab894b39ae4ff7c3f8a39d5b9
sukeypark <hspark@data-alliance.com>
2023-10-24 15:47:01 +0900

    rm unused

--- src/com/example/WeatherStation.java
+++ src/com/example/WeatherStation.java
@@ -1,15 +1,6 @@
 package com.example;
 
-import com.example.sensor.HumiditySensor;
-import com.example.sensor.PressureSensor;
-import com.example.sensor.Sensor;
-import com.example.sensor.TemperatureSensor;
-
 public class WeatherStation {
-
-    Sensor tempSensor = new TemperatureSensor();
-    Sensor pressureSensor = new PressureSensor();
-    Sensor humiditySensor = new HumiditySensor();
 
     public void measureData() {
         /* codes for measuring data */
